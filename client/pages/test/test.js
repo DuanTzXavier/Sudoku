eb6f1d8eaf3c70df411e04d4146085e65c66e690
tzduan <tzduan@Ctrip.com>
2017-12-19 19:43:46 +0800

    add select UI

--- client/pages/test/test.js
+++ client/pages/test/test.js
@@ -3,14 +3,15 @@ Page({
     listData: [
       { "code": "1", "text": "1", "type": "01" },
       { "code": "2", "text": "1", "type": "01" },
-      { "code": "03", "text": "01", "type": "01" },
+      { "code": "", "text": "01", "type": "01" },
       { "code": "04", "text": "01", "type": "01" },
       { "code": "05", "text": "01", "type": "01" },
       { "code": "06", "text": "01", "type": "01" },
       { "code": "07", "text": "01", "type": "01" },
       { "code": "07", "text": "01", "type": "01" },
       { "code": "07", "text": "01", "type": "01" }
-    ]
+    ],
+    viewBg: 'bisque'
   },
   onLoad: function () {
     console.log('onLoad')
@@ -19,6 +20,16 @@ Page({
   click: function(event) {
     console.log('onLoad')
     console.log(event)
+    if (event.target.dataset.hi != null){
+      wx.showToast({
+        title: event.target.dataset.hi,
+      })
+    }else{
+      wx.showToast({
+        title: "null",
+      })
+    }
+    
   }
 
 })
---
+++ client/pages/test2/test.js
@@ -0,0 +1,123 @@
+Page({
+
+  data: {
+    listData: {},
+    lastClick:undefined,
+    originListData: {},
+  },
+
+  onLoad: function () {
+    var datas = "200673045\n050020003\n000000009\n000000004\n000058001\n000017002\n000500428\n800000906\n600000007"
+    var convertData = this.convertListData(datas)
+    this.setData({
+      listData: convertData,
+      originListData: new Object(convertData)  
+    })
+  },
+
+  //构造闭包
+  convertListData: function (data) {
+    var dataList = new Array()
+    var dataLines = data.split("\n")
+    for (var line in dataLines){
+      var lineItems = new Array()
+      var dataLineSplit = dataLines[line].split("")
+      for (var lineItem in dataLineSplit){
+        var item = new Object()
+        if (dataLineSplit[lineItem] != 0){
+          item.number = dataLineSplit[lineItem]
+          item.editable = false
+        }else{
+          item.editable = true
+        }
+        if (this.checkEdgeSquared(lineItem, line)){
+          item.color = "burlywood"
+        }else{
+          item.color = "bisque"
+        }
+        item.position = lineItem + "" + line
+        lineItems.push(item)
+      }
+      var items = new Object()
+      items.items = lineItems
+      dataList.push(items)
+    }
+    return dataList
+  },
+
+  checkEdgeSquared: function (x, y){
+    if ((x > 2 && x < 6)&&(y <= 2)){
+      return true
+    }
+
+    if ((x >= 6) && (y > 2 && y < 6)) {
+      return true
+    }
+
+    if ((x <= 2) && (y > 2 && y < 6)) {
+      return true
+    }
+
+    if ((x > 2 && x < 6) && (y >= 6)) {
+      return true
+    }
+    return false
+  },
+
+  click: function (event) {
+
+    this.resumeLastClickPosition()
+
+    var position = event.currentTarget.dataset.hi.split("")
+    
+    this.data.lastClick = new Object()
+    this.data.lastClick.position = position
+    this.data.lastClick.color = this.data.listData[position[1]].items[position[0]].color
+
+    this.selectPosition(position)
+
+  },
+
+  checkEditable:function (position){
+    return this.data.listData[position[1]].items[position[0]].editable
+  },
+  resumeLastClickPosition: function(){
+    if (this.data.lastClick != undefined) {
+      this.setData({
+        listData: new Object(this.data.originListData)
+      })
+    }
+  },
+  selectPosition: function(position){
+    this.selectRow(position)
+    this.selectColumn(position)
+    this.selectSquared(position)
+    this.setData({
+      listData: this.data.listData
+    })
+  },
+  selectSquared: function(position){
+    var tempData = this.data.listData
+    var positionColumn = parseInt(position[0]/3)
+    var positionRow = parseInt(position[1]/3)
+    for (var x = positionRow * 3; x < positionRow * 3 + 3; x++){
+      for (var y = positionColumn * 3; y < positionColumn * 3 + 3; y++) {
+        this.data.listData[x].items[y].color = "darkgray"
+      }
+    }
+
+    console.log(positionRow)
+    console.log(positionColumn)
+  },
+  selectColumn: function (position){
+    for (var index = 0; index < 9; index++){
+      this.data.listData[index].items[position[0]].color = "darkgray"
+    }
+  },
+  selectRow: function (position){
+    var tempData = this.data.listData
+    for (var index = 0; index < 9; index++) {
+      this.data.listData[position[1]].items[index].color = "darkgray"
+    }
+  },
+})
